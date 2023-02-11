2bc1ef23c021726a00a7a513f890ba940e94f56d
Olga Buditska <olgabudickaja@gmail.com>
2023-02-11 15:36:32 +0200

    add navbar

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,9 +1,15 @@
 import './App.scss';
+import Navbar from "./components/navbar/Navbar";
+import { Container } from "@mui/material";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
     <div>
-      Home
+        <Navbar />
+        <Container maxWidth="xl" sx={{ minHeight: 'calc(100vh - 65px)' }}>
+            <AppRouter/>
+        </Container>
     </div>
   );
 }
---
+++ server/client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,84 @@
+import { Box, IconButton, Menu, MenuItem, Toolbar } from "@mui/material";
+import { AccountCircle, SearchOutlined } from "@mui/icons-material";
+import { Search, SearchIconWrapper, StyledInputBase, NavbarBg } from "./navbarStyle";
+import { useState } from "react";
+import { Logo, LogoContainer, Span, Title } from "./navbarStyled";
+import logo from "../../assets/logo.png";
+
+const Navbar = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const isMenuOpen = Boolean(anchorEl);
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const menuId = 'primary-search-account-menu';
+
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <NavbarBg position="static">
+                <Toolbar>
+                    <LogoContainer>
+                        <Logo src={logo} alt="logo"/>
+                        <Title>
+                            Lia<Span>Messenger</Span>
+                        </Title>
+                    </LogoContainer>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchOutlined />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                        <IconButton
+                            size="large"
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuOpen}
+                            color="inherit"
+                        >
+                            <AccountCircle sx={{ width: 48, height: 'auto' }} />
+                        </IconButton>
+                    </Box>
+
+                </Toolbar>
+            </NavbarBg>
+            {renderMenu}
+        </Box>
+    );
+};
+
+export default Navbar;
